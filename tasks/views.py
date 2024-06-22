9a33191d82de0ed3df0907c4da7df2d1e1ccfa22
Alisher <dautovalisher33@gmail.com>
2024-06-22 23:15:41 +0500

    Add TaskModelForm instance to context

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import TaskModel
+from .forms import TaskModelForm
+
 # Create your views here.
 def index(request):
     tasks = TaskModel.objects.all()
-    context = {'tasks': tasks}
+    form = TaskModelForm()
+    context = {'tasks': tasks, 'form': form}
 
     return render(request, 'tasks/index.html', context)
 
