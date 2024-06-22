6e9d77c3e4d3cb6b0499d505c549dbe751f63fab
Alisher <dautovalisher33@gmail.com>
2024-06-22 19:19:53 +0500

    Change view, return all tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import TaskModel
 # Create your views here.
 def index(request):
-    return HttpResponse('FUCK YOU')
+    tasks = TaskModel.objects.all()
+    context = {'tasks': tasks}
+
+    return render(request, 'tasks/index.html', context)
