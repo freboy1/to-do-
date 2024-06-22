26806ef1fce2786f074f3f9159af1fea2ee718d3
Alisher <dautovalisher33@gmail.com>
2024-06-22 23:03:03 +0500

    Fix url, delete extra comma

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('update/<int:id>/', views.update, name='update')
-],
+    path('update/<int:id>/', views.update, name='update'),
+]
