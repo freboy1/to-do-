ede0a939248e4b756d8390e151f17546b4090caa
Alisher <dautovalisher33@gmail.com>
2024-06-23 14:34:33 +0500

    Add url path for delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('update/<int:id>/', views.update, name='update'),
+    path('delete/<int:id>/', views.delete, name='delete'),
 ]
